efc3ea8fb172ff842f75acfc3f1e2bb613bb8de5
shion.t.fujie <tonaluna32966@gmail.com>
2020-04-13 19:17:58 +0900

    swap languages

--- js/content.js
+++ js/content.js
@@ -1,4 +1,29 @@
-document.onkeydown = ({ code }) => {
+document.onkeydown = event => {
   if (location.pathname != "/") return;
-  console.log(code);
+
+  const { code, shiftKey } = event;
+  if (shiftKey && code == "Digit1") {
+    swapLanguages();
+    event.preventDefault();
+  }
 };
+
+function swapLanguages() {
+  const b = document.querySelector(".swap");
+
+  const mouseoverE = document.createEvent("MouseEvents");
+  mouseoverE.initEvent("mouseover", true, false);
+  b.dispatchEvent(mouseoverE);
+
+  const mousedonwE = document.createEvent("MouseEvents");
+  mousedonwE.initEvent("mousedown", true, false);
+  b.dispatchEvent(mousedonwE);
+
+  const mouseupE = document.createEvent("MouseEvents");
+  mouseupE.initEvent("mouseup", true, false);
+  b.dispatchEvent(mouseupE);
+
+  const mouseoutE = document.createEvent("MouseEvents");
+  mouseoutE.initEvent("mouseout", true, false);
+  b.dispatchEvent(mouseoutE);
+}
